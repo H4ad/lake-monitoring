e070bea4d40ff74f48a195287c03bf034ad0dec8
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-06-09 20:15:43 -0300

    update(index): add implentation of reader and thinker

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // #region Imports
 
-import { SerialPort, parsers } from 'serialport';
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+
+const { config: thinker } = require('./config.js');
+
+const { writeFile } = require('jsonfile');
 
 // #endregion
 
-// #region Const Properties
+// #region Reader
+
+// #region Properties
 
 /**
  * A classe que lida com a leitura de dados serial
  */
-const port = new SerialPort('/dev/ttyACM0', {
-	baudrate: 9600,
-	parser: parsers.readline('\n'),
+const port = new SerialPort(config.port, {
+	parser: new Readline({ delimiter: '\r\n' }),
 });
 
 // #endregion
@@ -32,14 +38,39 @@ const initializeReadDataStream = () => {
 	port.on('data', onReadDataStream);
 };
 
+/**
+ * Método que envia dados falsos apenas para testar
+ */
+const fakeData = () => {
+	let counter = 1000;
+	let sinalNegative = false;
+
+	initializeThinker();
+
+	setInterval(() => {
+		if (sinalNegative) {
+			counter--;
+
+			sinalNegative = counter > 1000;
+		} else {
+			counter++;
+
+			sinalNegative = counter > 4000;
+		}
+
+		onReadDataStream(counter);
+	}, 1000);
+};
+
 // #endregion
 
-// #region Reader
+// #region Events
 
 /**
  * Método executado após abrir a conexão com a porta serial
  */
 const onOpenStream = () => {
+	initializeThinker();
 	initializeReadDataStream();
 };
 
@@ -49,9 +80,213 @@ const onOpenStream = () => {
  * @param {string} potentiometerPower A potência atual do potênciometro
  */
 const onReadDataStream = (potentiometerPower) => {
-	console.log(potentiometerPower);
+	const power = +potentiometerPower.toString();
+
+	if (isNaN(power))
+		return;
+
+	thinkerProcessMeasurement(power);
 };
 
 // #endregion
 
-initializeOpenStream();
+// #endregion
+
+// #region Thinker
+
+/**
+ * Type Defs do Thinker
+ *
+ * @typedef {{ value: string, time: number }} measurementData As informações de uma medição
+ * @typedef {{ isFirstProcess: boolean, measurementData[], measurementMaxToAlert: number }} thinkingData As informações de um pensamento
+ * @typedef {{ type: string, elevationAboveAverage: number, lastThinking: thinkingData, currentMeasurementData: measurementData[], time: number }} alertData As informações de um alerta
+ */
+
+// #region Properties
+
+/**
+ * O buffer de dados do Thinker
+ *
+ * @type {measurementData[]}
+ */
+let thinkerBufferData;
+
+/**
+ * O ultimo pensamento do Thinker
+ *
+ * @type {thinkingData}
+ */
+let thinkerLastThinking;
+
+/**
+ * A contagem até o Thinker criar um alerta
+ *
+ * @type {number}
+ */
+let thinkerDebounceAttemptsToAlert;
+
+/**
+ * O tempo até acabar o pensamento atual
+ *
+ * @type {number}
+ */
+let thinkerTimeToFinish;
+
+/**
+ * O tempo até habilitar o próximo alerta
+ *
+ * @type {number}
+ */
+let thinkerLastAlert = 0;
+
+// #endregion
+
+// #region Initializer
+
+/**
+ * Método que inicializa as configurações do Thinker
+ */
+const initializeThinker = () => {
+	thinkerBufferData = [];
+	thinkerLastThinking = {
+		isFirstProcess: true,
+		measurementData: [],
+		measurementMaxToAlert: 0,
+	};
+	thinkerDebounceAttemptsToAlert = 0;
+	thinkerTimeToFinish = (+new Date()) + thinker.timePerThinking;
+};
+
+// #endregion
+
+// #region Events
+
+/**
+ * Método que realiza o processo do Thinker
+ *
+ * @param {string} measurement O valor da medição
+ * @return {void}
+ */
+const thinkerProcessMeasurement = (measurement) => {
+	if ((+new Date()) > thinkerTimeToFinish)
+		return thinkerFinalizeThinking();
+
+	const measurementData = { value: +measurement, time: +new Date() };
+
+	thinkerBufferData.push(measurementData);
+
+	if (thinkerLastThinking.isFirstProcess)
+		return;
+
+	if (measurement < thinkerLastThinking.measurementMaxToAlert)
+		return thinkerDebounceAttemptsToAlert = 0;
+
+	thinkerDebounceAttemptsToAlert++;
+
+	console.log('Debounce de um alerta tentativa numero: ' + thinkerDebounceAttemptsToAlert);
+
+	if (thinkerDebounceAttemptsToAlert < thinker.debounceAttemptsToAlert)
+		return;
+
+	thinkerCreateAlert();
+};
+
+/**
+ * Método que finaliza um pensamento
+ */
+const thinkerFinalizeThinking = () => {
+	console.log('Finalizando um pensamento.');
+
+	if (thinkerBufferData.length === 0)
+		return;
+
+	const listMeasurement = thinkerBufferData.map((mesurementData) => mesurementData.value);
+
+	const media = listMeasurement.reduce((total, mesurement) => total + (mesurement / listMeasurement.length), 0);
+
+	const measurementMaxToAlert = media + thinker.securityThreshold;
+
+	thinkerLastThinking = {
+		isFirstProcess: false,
+		measurementData: thinkerBufferData,
+		measurementMaxToAlert,
+	};
+
+	thinkerDebounceAttemptsToAlert = 0;
+	thinkerBufferData = [];
+	thinkerTimeToFinish = (+new Date()) + thinker.timePerThinking;
+
+	saveThinkingToJson(thinkerLastThinking);
+};
+
+// #endregion
+
+/**
+ * Método que cria um alerta sobre a situação atual
+ */
+const thinkerCreateAlert = () => {
+	thinkerDebounceAttemptsToAlert = 0;
+
+	if ((+new Date()) <= thinkerLastAlert)
+		return;
+
+	thinkerLastAlert = thinkerTimeToFinish;
+
+	console.log('Criando um alerta');
+
+	if (thinkerBufferData.length === 0)
+		return;
+
+	const average = thinkerBufferData
+		.map((mesurementData) => mesurementData.value)
+		.reduce((total, value) => total + (value / thinkerBufferData.length), 0);
+
+	const elevationAboveAverage = average - thinkerLastThinking.measurementMaxToAlert;
+
+	const alertData = {
+		elevationAboveAverage,
+		type: 'danger',
+		lastThinking: thinkerLastThinking,
+		currentMeasurementData: thinkerBufferData,
+		time: +new Date(),
+	};
+
+	saveAlertToJson(alertData);
+};
+
+// #endregion
+
+// #region Writer
+
+/**
+ *	Método que salva os pensamentos para JSON
+ *
+ * @param {thinkingData} thinkingData
+ */
+const saveThinkingToJson = (thinkingData) => {
+	delete thinkingData.isFirstProcess;
+
+	const filename = `./thinking/thinking-${+new Date()}.json`;
+
+	writeFile(filename, thinkingData, { spaces: 2 })
+		.catch((error) => console.error(error));
+};
+
+/**
+ *	Método que salva os alertas para JSON
+ *
+ * @param {alertData} alertData
+ */
+const saveAlertToJson = (alertData) => {
+	const filename = `./alerts/alert-${alertData.type}-${+new Date()}.json`;
+
+	writeFile(filename, alertData, { spaces: 2 })
+		.catch((error) => console.error(error));
+};
+
+// #endregion
+
+if (config.isDebug)
+	fakeData();
+else
+	initializeOpenStream();
