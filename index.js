c18e4b3156e89b96d02fab36a40e5c71741416a8
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-06-09 15:11:56 -0300

    fix(index): problems

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-//#region Imports
+// #region Imports
 
 import { SerialPort, parsers } from 'serialport';
 
-//#endregion
+// #endregion
 
-//#region Const Properties
+// #region Const Properties
 
 /**
  * A classe que lida com a leitura de dados serial
  */
-const port = new SerialPort("/dev/ttyACM0", {
-    baudrate: 9600,
-    parser: parsers.readline("\n")
+const port = new SerialPort('/dev/ttyACM0', {
+  baudrate: 9600,
+  parser: parsers.readline('\n'),
 });
 
-//#endregion
+// #endregion
 
-//#region Methods
+// #region Methods
 
 /**
  * Método que abre a conexão com a porta serial
  */
 const initializeOpenStream = () => {
-    port.on("open", onOpenStream);
+  port.on('open', onOpenStream);
 };
 
 /**
  * Método executado após abrir a conexão com a porta serial
  */
 const onOpenStream = () => {
-    initializeReadDataStream();
+  initializeReadDataStream();
 };
 
 /**
  * Método que inicializa a leitura dos dados serial enviados pela placa
  */
 const initializeReadDataStream = () => {
-    port.on('data', onReadDataStream);
+  port.on('data', onReadDataStream);
 };
 
 /**
  * Método que recebe o valor da leitura do potênciometro
- * 
+ *
  * @param {string} potentiometerPower A potência atual do potênciometro
  */
 const onReadDataStream = (potentiometerPower) => {
-    console.log(potentiometerPower);
+  console.log(potentiometerPower);
 };
 
-//#endregion
+// #endregion
 
 initializeOpenStream();
